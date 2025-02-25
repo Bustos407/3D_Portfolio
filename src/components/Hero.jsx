0b730d813ebd0dc7b6788737216bd978c111dd6f
Santiago <bustosmoralesantiago@gmail.com>
2025-02-25 15:56:59 -0500

    Navbar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className="relative w-full h-screen mx-auto">
+      <div
+        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7x1 mx-auto flex flex-row items-start gap-5`}
+      >
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+        </div>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi, I'm <span className="text-[#915eff]">Santiago</span>
+          </h1>
+          <p className={`${styles.heroSubText}mt-2 text-white-100`}>
+            I am a full-stack developer with experience in both backend and
+            frontend development. Throughout my career, I have built technology
+            solutions from the ground up, showcasing my ability to work
+            independently and as part of a team. I am passionate about creating
+            robust, scalable, and user-focused applications, always striving to
+            deliver high-quality results that exceed expectations.
+            <br className="sm:block hidden" />
+          </p>
+        </div>
 
-export default Hero
+        
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
